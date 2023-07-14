216239a7c6af01b003ce06ac5162e99c724aae86
naderwael1 <naderwael39@gmail.com>
2023-07-13 17:47:40 -0700

    commmint

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+/**
+ * main - Entry pint
+ *
+ * Description: A C progtam that print with put function
+ *
+ * Return: always 0 (Success)
+*/
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}

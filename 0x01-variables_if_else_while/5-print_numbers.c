0a15aabfdd0c019b0410886a1f4c90ba76672513
naderwael1 <naderwael39@gmail.com>
2023-07-29 14:29:30 -0700

    commit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry pint
+ *
+ * Description: print all single digit number
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int digit = 0;
+
+	while (digi <= 9)
+	{
+		printf("%i", digit);
+		digit++;
+	}
+	printf("\n");
+
+	return (0);
+}

b1473278a21f84b307fe8e0716d2e6bb1edcd94d
naderwael1 <naderwael39@gmail.com>
2023-07-31 02:53:42 -0700

    commint

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -4,7 +4,7 @@
 /**
  * isLower - determines whether ascii is lowercase
  * @c: character
- * Return 1 if true , 0 if false
+ * Return: 1 if true , 0 if false
  */
 
 int isLower(char c)
@@ -27,3 +27,29 @@ int isDelimiter(char c)
 			return (1);
 	return (0);
 }
+/**
+ * cap_string - capitalizes all words of a string
+ * @s: input string
+ * Return: string with capitalized words
+ */
+
+chr *cap_string(char *s)
+{
+	char *ptr = s;
+	int foundDelimit = 1;
+
+	while (*s)
+	{
+		if (isDelimiter(*s))
+			foundDelimit = 1;
+		else if (isLower(*s) && foundDelimit)
+		{
+			*s -= 32;
+			foundDelimit = 0;
+		}
+		else
+			foundDelimit = 0;
+		s++;
+	}
+	return (ptr);
+
